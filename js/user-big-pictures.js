78b4320127d93469918b818b9174198ae4f76f9d
Nanammii <kaddia@gmail.com>
2023-03-08 22:02:58 +0700

    maked function close-open big pictures

--- js/user-big-pictures.js
+++ js/user-big-pictures.js
@@ -1,16 +1,25 @@
 import './photo-thumbnails.js';
 
 const userBigPictureElement = document.querySelector('.big-picture');
-const userBigPictureOpenElement = document.querySelectorAll('.picture');
+const picturesContainer = document.querySelector('.pictures');
+// const userBigPictureOpenElement = document.querySelectorAll('.picture');
 const userBigPictureCloseElement = document.querySelector('.big-picture__cancel');
 
-userBigPictureOpenElement.addEventListener('click', () => {
-  userBigPictureElement.classList.remove('hidden');
-});
+function onOpenElement (evt) {
+  if (evt.target.closest('.picture')) {
+    userBigPictureElement.classList.remove('hidden');
+  }
+}
 
-userBigPictureCloseElement.addEventListener('click', () => {
-  userBigPictureElement.classList.add('hidden');
-});
+function onCloseElement (evt) {
+  if (evt.target.closest('.big-picture__cancel')) {
+    userBigPictureElement.classList.add('hidden');
+  }
+}
+
+picturesContainer.addEventListener('click', onOpenElement);
+
+userBigPictureCloseElement.addEventListener('click', onCloseElement);
 
 document.addEventListener('keydown', (evt) => {
   if (evt.key === 'Escape') {
