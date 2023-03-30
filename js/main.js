5bff04eaa730a9bb5f955b1be08eca668f57a1aa
Nanammii <kaddia@gmail.com>
2023-03-30 14:00:58 +0700

    fixed load more Comments

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-import {createDetailsPhoto} from './data.js';
 import {renderGallery} from './big-pictures.js';
 import {renderThumbnails} from './photo-thumbnails.js';
 import {modalCloseHandler} from './form-modal.js';
 import {onFormSubmit} from './form.js';
 import {getData} from './api.js';
 
-renderGallery(createDetailsPhoto());
+// renderGallery(createDetailsPhoto());
 
 getData()
   .then((thumbnails) => {
     renderThumbnails(thumbnails);
+    renderGallery(thumbnails);
   });
 
 onFormSubmit(modalCloseHandler);
--- js/util.js
+++ js/util.js
@@ -59,4 +59,24 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export {createIdGenerator, getRandomInteger, createRandomId, getRandomArrayElement, isEscapeKey, showAlert};
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const throttle = (callback, delayBetweenFrames) => {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+};
+
+export {createIdGenerator, getRandomInteger, createRandomId,
+  getRandomArrayElement, isEscapeKey, showAlert, debounce, throttle};
